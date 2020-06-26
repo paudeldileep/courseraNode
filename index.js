3465ef7af3f9d1e4694fc3d755d516bcd361e01a
paudeldileep <dileepkpaudel@gmail.com>
2020-06-26 15:00:31 +0545

    express router

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express'),
 http = require('http');
 const morgan = require('morgan');    
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -10,6 +11,12 @@ const app = express();
 app.use(morgan('dev'));
 
 app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
+
+const dishRouter = require('./routes/dishRouter');
+
+app.use('/dishes', dishRouter);
 
 app.use((req, res, next) => {
   console.log(req.headers);
